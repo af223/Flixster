2ca42949db868a017290746c6179dfea386ee334
af223 <aimee.feng24@gmail.com>
2021-06-25 12:00:20 -0400

    Fixed formatting

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.flixster;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,10 +12,13 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterInside;
 import com.example.flixster.databinding.ActivityMovieDetailsBinding;
 import com.example.flixster.models.Movie;
 
 import org.parceler.Parcels;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -59,11 +63,27 @@ public class MovieDetailsActivity extends AppCompatActivity {
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage / 2.0f);
 
-        // backdrop for movie
-        Glide.with(this)
-                .load(movie.getBackdropPath()) // actual image
-                .placeholder(R.drawable.flicks_backdrop_placeholder) // loads when the image hasn't loaded yet
-                .into(tvBackdrop); // place image loads
+        // for rounded corners on images
+        int radius = 20; // corner radius, higher value = more rounded
+        int margin = 5; // crop margin, set to 0 for corners with no crop
+
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // backdrop for movie
+            Glide.with(this)
+                    .load(movie.getBackdropPath()) // actual image
+                    .placeholder(R.drawable.flicks_backdrop_placeholder) // loads when the image hasn't loaded yet
+                    .transform(new RoundedCornersTransformation(radius, margin))
+                    .into(tvBackdrop); // place image loads
+        }
+        else {
+            // backdrop for movie
+            Glide.with(this)
+                    .load(movie.getBackdropPath()) // actual image
+                    .placeholder(R.drawable.flicks_backdrop_placeholder) // loads when the image hasn't loaded yet
+                    .centerCrop() // scale image to fill the entire ImageView
+                    .transform(new CenterInside(), new RoundedCornersTransformation(radius, margin))
+                    .into(tvBackdrop); // place image loads
+        }
     }
 
     public void onTrailerClick(View view) {
